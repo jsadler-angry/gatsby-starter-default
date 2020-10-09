4ea69007be1e3c16af1e25bf9af12959b1f0ca63
Jim Sadler <jimsadler@Jims-MacBook-Pro.local>
2020-10-09 13:51:54 -0400

    added query

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,22 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/assets`,
+        name: "images",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    "gatsby-transformer-remark",
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,43 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
+import { graphql } from "gatsby"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people new</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+export const query = graphql`
+  query {
+    allFile(
+      filter: { sourceInstanceName: { eq: "content" }, name: { eq: "index" } }
+    ) {
+      edges {
+        node {
+          childMarkdownRemark {
+            frontmatter {
+              image
+              intro
+              title
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
+const IndexPage = props => {
+  const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>{data.title}</h1>
+      <p>{data.intro}</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <img src={data.image} alt="" />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  )
+}
 export default IndexPage
