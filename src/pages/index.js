8a94ac8cafa4c5b126ba1e22c9d9ef01b3b5f72d
Jim Sadler <jimsadler@Jims-MacBook-Pro.local>
2020-10-10 12:45:51 -0400

    added about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,7 +47,7 @@ const IndexPage = props => {
         <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
           <img src={data.image} alt="" />
         </div>
-        <Link to="/page-2/">Go to page 2</Link> <br />
+        <Link to="/about/">Go to page 2</Link> <br />
         <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
       </Layout>
     </div>
